4a3bdeaed72a305930162964079257d84d63fa13
Vu Ha <stregawolf@gmail.com>
2015-08-22 20:07:18 -0700

    Added boat class and buoyancy, tuned the serpent drag some more

---
+++ SuperSeaSerpentSimSix/Assets/SSSS/Scripts/Gameplay/Boat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour {
+
+	public Buoyancy mBuoyancy;
+
+	public float mCapsizeTime = 2.0f;
+	protected float mCapsizeTimer = 0.0f;
+
+	public void Awake()
+	{
+		if(mBuoyancy == null)
+		{
+			mBuoyancy = GetComponent<Buoyancy>();
+		}
+	}
+
+	public void Update()
+	{
+		Vector3 down = transform.position.normalized*-1;
+
+		if(Vector3.Dot(down, transform.up) > 0)
+		{
+			mCapsizeTimer += Time.deltaTime;
+			if(mCapsizeTimer >= mCapsizeTime)
+			{
+				mBuoyancy.mBuoyancyEnabled = false;
+			}
+		}
+		else
+		{
+			mCapsizeTimer = 0.0f;
+		}
+	}
+}
---
+++ SuperSeaSerpentSimSix/Assets/SSSS/Scripts/Gameplay/Buoyancy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buoyancy  : MonoBehaviour {
+
+	public static float kGravity = 9.8f;
+
+	public bool mBuoyancyEnabled = true;
+	public float mBuoyanceFactor = 0.25f;
+	public float mEndsOffset = 1.0f;
+
+	public Rigidbody mRigidbody;
+
+	public void Awake()
+	{
+		if(mRigidbody == null)
+		{
+			mRigidbody = GetComponent<Rigidbody>();
+		}
+	}
+
+	public void FixedUpdate () 
+	{
+		HandleBuoyancy(transform.position + transform.right*mEndsOffset, 0.5f);
+		HandleBuoyancy(transform.position - transform.right*mEndsOffset, 0.5f);
+	}
+
+	public void HandleBuoyancy(Vector3 worldPos, float optForceContribution = 1.0f)
+	{
+		float distFromSeaLevel = World.Instance.SeaLevel-0.5f - worldPos.magnitude;
+		Vector3 gravityDir = transform.position;
+		gravityDir.Normalize();
+		gravityDir *= optForceContribution;
+
+		if(distFromSeaLevel < 0 || !mBuoyancyEnabled)// above water
+		{
+			mRigidbody.AddForceAtPosition(gravityDir*-kGravity, worldPos, ForceMode.Acceleration);
+		}
+		else
+		{
+			mRigidbody.AddForceAtPosition(gravityDir*kGravity*distFromSeaLevel*mBuoyanceFactor, worldPos, ForceMode.Acceleration);
+		}
+	}
+}
