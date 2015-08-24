d2610251b5009dc8381772ffeda398f66f445519
Ryan Ingram <ringram@ea.com>
2015-08-24 00:47:19 -0700

    version number, ui tuning

---
+++ SuperSeaSerpentSimSix/Assets/SSSS/Scripts/UI/VersionText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VersionText : MonoBehaviour
+{
+    public const string Version = "v0.197";
+
+	void Start ()
+    {
+        GetComponent<Text>().text = Version;
+	}
+}
