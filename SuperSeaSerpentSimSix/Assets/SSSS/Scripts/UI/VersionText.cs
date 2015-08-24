682e6fab803861bed1b83ab93e7f5ee4677a2c18
Ryan Ingram <ringram@ea.com>
2015-08-24 01:08:38 -0700

    up version #

--- SuperSeaSerpentSimSix/Assets/SSSS/Scripts/UI/VersionText.cs
+++ SuperSeaSerpentSimSix/Assets/SSSS/Scripts/UI/VersionText.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class VersionText : MonoBehaviour
 {
-    public const string Version = "v0.197";
+    public const string Version = "v0.198";
 
 	void Start ()
     {
